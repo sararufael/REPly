21b4d473b065782ebbc963cc238431877328fd17
Sara <sara.rufael@gmail.com>
2018-10-21 02:50:08 -0400

    last commit of night

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
-from mockdata import mockdata
 
 from flask import Flask, render_template, request
 import requests
@@ -13,19 +9,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/yourrep',methods=['POST'])
+@app.route('/destination',methods=['POST'])
 def repinformation():
     address = request.form['address']
     data = requests.get('https://www.googleapis.com/civicinfo/v2/representatives?address='+address+'&key=AIzaSyByalwgte_IJ-iClnTKSJVdWv7dxhIdLhU')
     reps_json = data.json()
     user_info = reps_json['normalizedInput']
 # index order: US Senator, US Senator, US Representative, State Senator, State Representative
-    officials_list = {reps_json['officials'][2],reps_json['officials'][3],reps_json['officials'][4],reps_json['officials'][7],reps_json['officials'][8]}
-    return render_template('yourrep.html', data=officials_list)
+    officials_list = [reps_json['officials'][0],reps_json['officials'][2],reps_json['officials'][3],reps_json['officials'][4],reps_json['officials'][7],reps_json['officials'][8]]
+    return render_template('destination.html', officials_list=officials_list)
 
 @app.route('/representative')
 def representative():
     return render_template('representative.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('blog.html')
+
+@app.route('/issue')
+def issue():
+    return render_template('issue.html')
 
 
 if __name__ == '__main__':
